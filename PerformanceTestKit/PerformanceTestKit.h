67a342b4b73e08eae71ff851c81bdea1812208be
Lucas Stomberg <lstomberg@gmail.com>
2017-07-08 16:05:03 -0500

    Adding project

---
+++ PerformanceTestKit/PerformanceTestKit.h
@@ -0,0 +1,19 @@
+//
+//  PerformanceTestKit.h
+//  PerformanceTestKit
+//
+//  Created by Lucas Stomberg on 7/8/17.
+//  Copyright © 2017 Lucas Stomberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PerformanceTestKit.
+FOUNDATION_EXPORT double PerformanceTestKitVersionNumber;
+
+//! Project version string for PerformanceTestKit.
+FOUNDATION_EXPORT const unsigned char PerformanceTestKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PerformanceTestKit/PublicHeader.h>
+
+
