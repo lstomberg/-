2bdf3c1cbc3e78b14f1ee92e55d1564c06778d68
Lucas Stomberg <lstomberg@gmail.com>
2017-07-10 05:43:47 -0500

    config files

--- Sources/PerformanceTestKit.h
+++ Sources/PerformanceTestKit.h
@@ -13,8 +13,3 @@ FOUNDATION_EXPORT double PerformanceTestKitVersionNumber;
 
 //! Project version string for PerformanceTestKit.
 FOUNDATION_EXPORT const unsigned char PerformanceTestKitVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <PerformanceTestKit/PublicHeader.h>
-#import <PerformanceTestKit/PerformanceLog.h>
-#import <PerformanceTestKit/Report-Swift.h>
-#import <PerformanceTestKit/Macros-ObjectiveC.h>
